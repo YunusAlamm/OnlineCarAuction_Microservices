43fde813befe0b47f678cad611330259ad759252
Yunus Alamdari <alamdari.yoonos@gmail.com>
2025-08-04 09:47:58 +0330

    Create SearchController.cs

---
+++ src/SearchService/Controllers/SearchController.cs
@@ -0,0 +1,29 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+using SearchService.Infrastructure.DatabaseModel;
+
+namespace SearchService.Controllers;
+
+[ApiController]
+[Route("api/search")]
+public class SearchController : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<List<Item>>> SearchItem(string searchTerm)
+    {
+        var query = DB.Find<Item>();
+        query.Sort(x => x.Ascending(a => a.Make));
+
+        if (!string.IsNullOrEmpty(searchTerm))
+        {
+            query.Match(Search.Full, searchTerm).SortByTextScore();
+        }
+        var result = await query.ExecuteAsync();
+
+        return result;
+            
+
+    } 
+
+}
