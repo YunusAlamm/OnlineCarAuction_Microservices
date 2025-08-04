b83c1bf8b1856bd33d6e4790800cf5181d26558d
Yunus Alamdari <alamdari.yoonos@gmail.com>
2025-08-04 09:48:20 +0330

    Add MongoDB item model and database seeding
    Introduces the Item entity for MongoDB, a DbInitializer to seed initial auction data from auctions.json, and updates Program.cs to initialize and seed the database on startup. This sets up the foundation for storing and searching auction items in the SearchService.

---
+++ src/SearchService/Infrastructure/Data/DbInitializer.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Text.Json;
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Infrastructure.DatabaseModel;
+
+namespace SearchService.Infrastructure.Data;
+
+public class DbInitializer
+{
+    public static async Task InitDb(WebApplication app)
+    {
+        await DB.InitAsync("SearchDb", MongoClientSettings
+.FromConnectionString(app.Configuration.GetConnectionString("MongoDbConnection")));
+
+
+        await DB.Index<Item>()
+            .Key(x => x.Make, KeyType.Text)
+            .Key(x => x.Model, KeyType.Text)
+            .Key(x => x.Color, KeyType.Text)
+            .CreateAsync();
+
+        var count = await DB.CountAsync<Item>();
+        if (count == 0)
+        {
+            Console.WriteLine("Seeding database with initial data...");
+            var itemData = await File.ReadAllTextAsync("Infrastructure/Data/auctions.json");
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
+
+            await DB.SaveAsync(items);
+
+
+        }
+    }
+    
+    
+
+}
--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,8 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Infrastructure.Data;
+using SearchService.Infrastructure.DatabaseModel;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,5 +19,18 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+try
+{
+    await DbInitializer.InitDb(app);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"An error occurred while initializing the database: {ex.Message}");
+    throw;
+    
+}
+   
+
 
 app.Run();
