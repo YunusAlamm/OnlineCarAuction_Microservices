477bb9582a51cb19637308cbce0ca944bd2ab9fc
Yunus Alamdari <alamdari.yoonos@gmail.com>
2025-08-05 17:24:06 +0330

    Add Polly retry policy to AuctionSvcHttpClient To make http comunication resilience

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,5 +1,7 @@
 using MongoDB.Driver;
 using MongoDB.Entities;
+using Polly;
+using Polly.Extensions.Http;
 using SearchService.Infrastructure.Data;
 using SearchService.Infrastructure.DatabaseModel;
 using SearchService.Infrastructure.SvcComunications;
@@ -9,7 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddHttpClient<AuctionSvcHttpClient>();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetRetryPolicy());
 
 var app = builder.Build();
 
@@ -21,17 +23,28 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-try
+app.Lifetime.ApplicationStarted.Register(async () =>
 {
-    await DbInitializer.InitDb(app);
-}
-catch (Exception ex)
-{
-    Console.WriteLine($"An error occurred while initializing the database: {ex.Message}");
-    throw;
+    try
+    {
+        await DbInitializer.InitDb(app);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"An error occurred while initializing the database: {ex.Message}");
+        throw;
     
-}
+    }
+});
    
 
 
 app.Run();
+
+
+static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+    => HttpPolicyExtensions
+    .HandleTransientHttpError()
+    .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
+    .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
+
