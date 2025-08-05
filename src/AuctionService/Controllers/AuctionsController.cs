f8f858d736f389f0f253f11ef4a323227868a7bc
Yunus Alamdari <alamdari.yoonos@gmail.com>
2025-08-05 16:41:31 +0330

    Add date filter to GetAllAuctions endpoint
    with 'date' parameter we can retrieve the newly added or updated auctions from auction service to save them in SearchService DataBase

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -3,6 +3,7 @@ using AuctionService.Application.DTOs;
 using AuctionService.Domain.Entities;
 using AuctionService.Infrastructure.Data;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -22,13 +23,16 @@ public class AuctionsController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions()
+    public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions(string date)
     {
-        var auctions = await _dbContext.Auctions
-            .Include(a => a.Item)
-            .OrderBy(x => x.Item.Make)
-            .ToListAsync();
-        return _mapper.Map<List<AuctionDto>>(auctions);
+        var query = _dbContext.Auctions.OrderBy(x => x.Item.Make).AsQueryable();
+        if (!string.IsNullOrEmpty(date))
+        {
+            query = query.Where(x => x.UpdatedAt.CompareTo(DateTime.Parse(date).ToUniversalTime()) > 0);
+        }
+
+
+        return await query.ProjectTo<AuctionDto>(_mapper.ConfigurationProvider).ToListAsync(); 
     }
 
     [HttpGet("{id}")]
