75029d94a26ba081609d46c8b98266eed854d051
Yunus Alamdari <alamdari.yoonos@gmail.com>
2025-09-01 01:58:04 +0330

    Update HostingExtensions to Handle HTTP protocol instead of HTTPS

--- src/IdentityService/HostingExtensions.cs
+++ src/IdentityService/HostingExtensions.cs
@@ -77,6 +77,11 @@ internal static class HostingExtensions
             .AddAspNetIdentity<ApplicationUser>()
             .AddLicenseSummary();
 
+            builder.Services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.SameSite = SameSiteMode.Lax;
+            });
+
         builder.Services.AddAuthentication();
 
         return builder.Build();
