191980506cc420b2cd0f5e69bfd1ecf6a5e422f5
Yunus Alamdari <alamdari.yoonos@gmail.com>
2025-09-27 14:00:42 +0330

    Styling AuctionCard

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -2,7 +2,7 @@ import AuctionCard from "./AuctionCard";
 
 
 async function getData(){
-    const res = await fetch('http://localhost:6001/search');
+    const res = await fetch('http://localhost:6001/search?pagesize=10');
 
     if(!res.ok) throw new Error('Failed to fetch data');
 
@@ -16,7 +16,7 @@ export default async function Listings() {
   const data = await getData();
 
   return (
-    <div>
+    <div className="grid grid-cols-4 gap-6">
       {data && data.results.map((auction: any) =>(
         <AuctionCard key={auction.id} auction={auction}/>
       ))}
