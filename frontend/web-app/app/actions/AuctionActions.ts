eae0a96591336020348741a3e5f7a5b2ef71130b
Yunus Alamdari <alamdari.yoonos@gmail.com>
2025-09-28 17:47:48 +0330

    Refactoring Code to use zustand state

--- frontend/web-app/app/actions/AuctionActions.ts
+++ frontend/web-app/app/actions/AuctionActions.ts
@@ -1,8 +1,8 @@
 'use server'
 import { PagedResult, Auction } from "@/types";
 
-export async function getData(pageNumber : number, pageSize: number): Promise<PagedResult<Auction>> {
-    const res = await fetch(`http://localhost:6001/search?pagesize=${pageSize}&pageNumber=${pageNumber}`);
+export async function getData(query: string): Promise<PagedResult<Auction>> {
+    const res = await fetch(`http://localhost:6001/search${query}`);
 
     if (!res.ok) throw new Error('Failed to fetch data');
 
