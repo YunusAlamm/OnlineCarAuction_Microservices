1eccb51ededbfa656c80698b66e8c7e9df03c503
Yunus Alamdari <alamdari.yoonos@gmail.com>
2025-10-01 21:15:46 +0330

    Creating AuctionForm part2

--- frontend/web-app/app/auctions/AuctionForm.tsx
+++ frontend/web-app/app/auctions/AuctionForm.tsx
@@ -1,7 +1,58 @@
+'use client'
+import { Button, HelperText, Spinner, TextInput } from 'flowbite-react';
+import { useRouter } from 'next/navigation';
 import React from 'react'
+import { FieldValues, useForm } from 'react-hook-form';
 
 export default function AuctionForm() {
-  return (
-    <div>Auction Form Goes here</div>
-  )
+    const router = useRouter();
+    const { register, handleSubmit, setFocus,
+        formState: { isSubmitting, isValid, isDirty, errors } } = useForm();
+
+    function onSubmit(data: FieldValues) {
+        console.log(data);
+    }
+
+    return (
+        <form className='flex flex-col mt-3' onSubmit={handleSubmit(onSubmit)}>
+            <div className='mb-3 block'>
+                <TextInput
+                    {...register('make', { required: 'Make is required' })}
+                    placeholder='Make'
+                    color={errors.make && 'failure'}
+
+                />
+                <HelperText color="failure">
+                    {errors.make?.message as string}
+                </HelperText>
+            </div>
+
+            <div className='mb-3 block'>
+                <TextInput
+                    {...register('model', { required: 'model is required' })}
+                    placeholder='Model'
+                    color={errors.model && 'failure'}
+
+                />
+                <HelperText color="failure">
+                    {errors.model?.message as string}
+                </HelperText>
+            </div>
+
+            <div className='flex justify-between'>
+                <Button color="alternative" onClick={() => router.push('/')}>Cancel</Button>
+                <Button
+                    outline
+                    color='green'
+                    type='submit'
+                    disabled={!isValid || !isDirty}
+                >
+                    {isSubmitting && <Spinner size='sm' />}
+                    Submit
+
+                </Button>
+
+            </div>
+        </form>
+    )
 }
--- frontend/web-app/app/nav/UserActions.tsx
+++ frontend/web-app/app/nav/UserActions.tsx
@@ -7,22 +7,28 @@ import { signOut } from 'next-auth/react'
 import Link from 'next/link'
 import { AiFillCar, AiFillTrophy, AiOutlineLogout } from 'react-icons/ai'
 import { HiCog, HiUser } from 'react-icons/hi'
+import { usePathname, useRouter } from 'next/navigation'
 
 type Props = {
   user: ExtendedUser
 }
+
 export default function UserActions({ user }: Props) {
+  const router = useRouter();
+  const pathname = usePathname();
   const setParams = useParamsStore(state => state.setParams);
 
   function setWinner(){
     if (user.username) {
       setParams({winner: user.username, seller: undefined})
+      if (pathname !== '/') router.push('/' );
     }
   }
 
   function setSeller(){
     if (user.username) {
       setParams({seller: user.username, winner: undefined});
+      if (pathname !== '/') router.push('/');
     }
   }
 
