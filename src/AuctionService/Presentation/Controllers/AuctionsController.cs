7060aa3bdd475363b04c03244224e59b2fc46efd
Yunus Alamdari <alamdari.yoonos@gmail.com>
2025-10-06 08:08:19 +0330

    Adding gRPC part 1

--- src/AuctionService/Presentation/Controllers/AuctionsController.cs
+++ src/AuctionService/Presentation/Controllers/AuctionsController.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace AuctionService.Controllers;
+namespace AuctionService.Presentation.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
