1b719dd3a4dd688315341a1e6741c5f06759f8f6
Yunus Alamdari <167519789+YunusAlamm@users.noreply.github.com>
2025-10-11 16:53:14 +0330

    Add Conditionals to the bid form

--- frontend/web-app/app/auctions/details/[id]/BidList.tsx
+++ frontend/web-app/app/auctions/details/[id]/BidList.tsx
@@ -61,7 +61,20 @@ export default function BidList({ user, auction }: Props) {
 
 
             <div className=" px-2 pb-2 text-gray-500">
-                <BidForm auctionId={auction.id} highBid={highBid} />
+                {!user ? (
+                    <div className="flex items-center justify-center p-2 text-lg font-semibold ">
+                        please login to make a bid
+
+                    </div>
+                ): user && user.name === auction.seller ? (
+                        <div className="flex items-center justify-center p-2 text-lg font-semibold ">
+                            You cannot bid on you own auction
+
+                        </div>
+                ): (
+
+                    <BidForm auctionId={auction.id} highBid={highBid} />
+                )}
             </div>
 
 
