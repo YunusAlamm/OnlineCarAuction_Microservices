9102d4b8fc976587c45fc3babe9c5fbe72062d99
Yunus Alamdari <167519789+YunusAlamm@users.noreply.github.com>
2025-11-11 20:53:40 +0330

    Setting Details of CustomWebAppFactory

--- tests/AuctionService.IntegrationTests/Fixtures/CustomWebAppFactory.cs
+++ tests/AuctionService.IntegrationTests/Fixtures/CustomWebAppFactory.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using AuctionService.Infrastructure.Data;
+using MassTransit;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Testcontainers.PostgreSql;
 
 namespace AuctionService.IntegrationTests.Fixtures
@@ -6,14 +12,38 @@ namespace AuctionService.IntegrationTests.Fixtures
     public class CustomWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
     {
         private PostgreSqlContainer _postgreSqlContainer = new PostgreSqlBuilder().Build();
-        public Task InitializeAsync()
+        public async Task InitializeAsync()
         {
-            throw new NotImplementedException();
+            await _postgreSqlContainer.StartAsync();
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureTestServices(services =>
+            {
+                var discriptor = services.SingleOrDefault(d =>
+                    d.ServiceType == typeof(DbContextOptions<AuctionDbContext>));
+
+                if(discriptor != null) services.Remove(discriptor);
+
+                services.AddDbContext<AuctionDbContext>(options =>
+                {
+                    options.UseNpgsql(_postgreSqlContainer.GetConnectionString());
+                });
+
+                services.AddMassTransitTestHarness();
+
+                var sp = services.BuildServiceProvider();
+                using var scope = sp.CreateScope(); 
+                var scopedServices = scope.ServiceProvider;
+                var db = scopedServices.GetRequiredService<AuctionDbContext>();
+                db.Database.Migrate();
+            });
         }
 
         Task IAsyncLifetime.DisposeAsync()
         {
-            throw new NotImplementedException();
+            return _postgreSqlContainer.DisposeAsync().AsTask();
         }
     }
 }
