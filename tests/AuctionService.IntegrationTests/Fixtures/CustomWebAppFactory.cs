140951fb18d3fba78610492de297100f69dd658d
Yunus Alamdari <167519789+YunusAlamm@users.noreply.github.com>
2025-11-11 20:50:46 +0330

    Setting up a Custom web application factory for integration testing

--- tests/AuctionService.IntegrationTests/Fixtures/CustomWebAppFactory.cs
+++ tests/AuctionService.IntegrationTests/Fixtures/CustomWebAppFactory.cs
@@ -1,13 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc.Testing;
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Testcontainers.PostgreSql;
 
 namespace AuctionService.IntegrationTests.Fixtures
 {
-    public class CustomWebAppFactory : WebApplicationFactory<Program>
+    public class CustomWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
     {
+        private PostgreSqlContainer _postgreSqlContainer = new PostgreSqlBuilder().Build();
+        public Task InitializeAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IAsyncLifetime.DisposeAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
